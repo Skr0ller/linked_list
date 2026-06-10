d8ede7ebe52fe25ddd8ea75c9136c34690c500f0
Skr0ller <mryarikromanuk@gmail.com>
2026-06-10 11:43:57 +0300

    include commands fixed

--- source/comm.c
+++ source/comm.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../include/main.h"
-#include "../include/comm.h"
+#include "include/main.h"
+#include "include/comm.h"
 
 void getnode(char *nid, inode *head_node)
 {
